8ca727148885eb9e7ae5a1dac8fbfec4d12a4faa
jmathews <jmathews@localhost.localdomain>
2014-07-19 00:52:38 +0530

    before item styling

---
+++ menuItems.js
@@ -0,0 +1,78 @@
+var menu=[{"HKB" : [
+	{
+	"category_name" : "chinese",
+	"category_content" : [
+		{
+		"name" : "noodlasasas asasaases",
+		"cost" : "40.50",
+		"class" : "veg",
+		"freq" : "0"
+		},
+		{
+		"name" : "noodles",
+		"cost" : "40.50",
+		"class" : "veg",
+		"freq" : "0"
+		},
+		{
+		"name" : "noodles",
+		"cost" : "40.50",
+		"class" : "veg",
+		"freq" : "0"
+		}
+		]
+	},
+	{
+	"category_name" : "chinese",
+	"category_content" : [
+		{
+		"name" : "noodles",
+		"cost" : "40.50",
+		"class" : "veg",
+		"freq" : "0"
+		},
+		{
+		"name" : "noodles",
+		"cost" : "40.50",
+		"class" : "veg",
+		"freq" : "0"
+		},
+		{
+		"name" : "noodles",
+		"cost" : "40.50",
+		"class" : "veg",
+		"freq" : "0"
+		},
+		{
+		"name" : "noodles",
+		"cost" : "40.50",
+		"class" : "veg",
+		"freq" : "0"
+		},
+		{
+		"name" : "noodles",
+		"cost" : "40.50",
+		"class" : "veg",
+		"freq" : "0"
+		},
+		{
+		"name" : "noodles",
+		"cost" : "40.50",
+		"class" : "veg",
+		"freq" : "0"
+		}
+		]
+	},
+	{
+	"subject_name" : "biology",
+	"exps" : [
+		]
+	},
+	{
+	"subject_name" : "mathematics",
+	"exps" : [
+	]
+	}
+]
+}];
+JSON.stringify(menu);
