de8fb4cd0f9af496a127a8abc5b1aefc8500c026
root <root@localhost.localdomain>
2014-07-21 21:55:44 +0530

    profile start

--- menuItems.js
+++ menuItems.js
@@ -12,7 +12,7 @@ var menu =[{"HKB" : [
 
 
 
-list={'it1':2 , 'it2':3 , 'it3':4 , 'it4':1};
+var list={'it1':2 , 'it2':3 , 'it3':4 , 'it4':1};
 
 
 jack={'it1':{'name':'orange','cost':20 } , 'it2':{'name':'pinaaple','cost':15 } ,'it3':{'name':'orange','cost':20 } ,'it4':{'name':'orange','cost':20 },'it5':{'name':'orange','cost':20 },'it6':{'name':'orange','cost':20 },'it7':{'name':'orange','cost':20 }};
